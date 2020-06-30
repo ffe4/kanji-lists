c56b1aec4e61502b4665d3f8a1bb1b4d026923d9
Daniel Lemm <61800298+ffe4@users.noreply.github.com>
2020-06-30 09:31:06 +0200

    Add code generation for __init__.py

--- generate.py
+++ generate.py
@@ -1,6 +1,7 @@
 import json
 
 from jinja2 import Template
+from tomlkit import parse
 
 with open("codegen/kanji_lists.json") as f:
     data = json.load(f)
@@ -10,3 +11,13 @@ with open("codegen/kanji_lists.jinja2") as f:
 
 with open("kanji_lists/kanji_lists.py", "w") as f:
     f.write(template.render(data=data, set=set))
+
+with open("pyproject.toml") as f:
+    doc = parse(f.read())
+project_version = doc["tool"]["poetry"]["version"]
+
+with open("codegen/__init__.jinja2") as f:
+    template = Template(f.read())
+
+with open("kanji_lists/__init__.py", "w") as f:
+    f.write(template.render(data=data, version=project_version))
--- kanji_lists/__init__.py
+++ kanji_lists/__init__.py
@@ -1,3 +1,6 @@
-from kanji_lists.kanji_lists import JOYO, JINMEIYO, KYOIKU, JLPT
+"""This module has been automatically generated. Do not edit manually!"""
+
+
+from kanji_lists.kanji_lists import JINMEIYO, JLPT, JOYO, KYOIKU
 
 __version__ = "0.1.0"
